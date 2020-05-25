747ce777037a5edfa342340ce5e85b275f556264
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 09:35:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library
   loaded_library = YAML.load_file('lib/emoticons.yml')
   new_library = {}
   
-  loaded_library.each { |emotion|
+  loaded_library.each { |emotion, translation|
     new_library[emotion] = {
       english: loaded_library[emotion][0],
       japanese: loaded_library[emotion][1]
